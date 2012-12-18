e2920f296ec150d9195149aa1d192fe6223cf505
pete <pete@unigo.com>
2012-12-18 17:50:06 -0500

    organization model initial

---
+++ AFA.Service/Models/Organization.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AFA.Service.Models
+{
+    public class Organization
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<OrganizationCategory> Categories { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string City { get; set; }
+        public StateProvince State { get; set; }
+        public string Zipcode { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string WebpageUrl { get; set; }
+        public string Description { get; set; }
+    }
+}
