4e7a7813c904bb33e408bc10662d05cfb30fc000
pjacko <pdangelo22@gmail.com>
2013-01-14 23:38:59 -0500

    enhancements to organization android layout

--- AFA.Android/Resources/Resource.Designer.cs
+++ AFA.Android/Resources/Resource.Designer.cs
@@ -64,13 +64,22 @@ namespace AFA.Android
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int calendarIcon = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int list_selector = 2130837505;
+			public const int Icon = 2130837505;
 			
 			// aapt resource value: 0x7f020002
-			public const int sharkFin = 2130837506;
+			public const int list_selector = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int newsIcon = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int sharkFin = 2130837508;
+			
+			// aapt resource value: 0x7f020005
+			public const int speechIcon = 2130837509;
 			
 			private Drawable()
 			{
@@ -80,62 +89,86 @@ namespace AFA.Android
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f06000d
-			public const int AddressButton = 2131099661;
+			// aapt resource value: 0x7f060015
+			public const int AddressButton = 2131099669;
+			
+			// aapt resource value: 0x7f060019
+			public const int Allies = 2131099673;
+			
+			// aapt resource value: 0x7f06000e
+			public const int CalendarIconButton = 2131099662;
+			
+			// aapt resource value: 0x7f060018
+			public const int CityState = 2131099672;
 			
 			// aapt resource value: 0x7f060011
-			public const int Allies = 2131099665;
+			public const int CommentsButton = 2131099665;
 			
-			// aapt resource value: 0x7f060010
-			public const int CityState = 2131099664;
-			
-			// aapt resource value: 0x7f060009
-			public const int CommentsButton = 2131099657;
-			
-			// aapt resource value: 0x7f06000a
-			public const int ContactLabel = 2131099658;
-			
-			// aapt resource value: 0x7f060005
-			public const int DescriptionText = 2131099653;
-			
-			// aapt resource value: 0x7f06000f
-			public const int EditOrgButton = 2131099663;
-			
-			// aapt resource value: 0x7f06000b
-			public const int EmailButton = 2131099659;
+			// aapt resource value: 0x7f060012
+			public const int ContactLabel = 2131099666;
 			
 			// aapt resource value: 0x7f060008
-			public const int EventsButton = 2131099656;
+			public const int DescriptionText = 2131099656;
 			
-			// aapt resource value: 0x7f060006
-			public const int FollowingButton = 2131099654;
+			// aapt resource value: 0x7f060017
+			public const int EditOrgButton = 2131099671;
+			
+			// aapt resource value: 0x7f060013
+			public const int EmailButton = 2131099667;
+			
+			// aapt resource value: 0x7f06000f
+			public const int EventsButton = 2131099663;
+			
+			// aapt resource value: 0x7f060009
+			public const int FollowingButton = 2131099657;
 			
 			// aapt resource value: 0x7f060000
 			public const int MyButton = 2131099648;
 			
-			// aapt resource value: 0x7f060003
-			public const int NeedVolunteersLabel = 2131099651;
-			
-			// aapt resource value: 0x7f060007
-			public const int NewsButton = 2131099655;
-			
-			// aapt resource value: 0x7f060001
-			public const int OrgImage = 2131099649;
-			
-			// aapt resource value: 0x7f060002
-			public const int OrgName = 2131099650;
-			
-			// aapt resource value: 0x7f060012
-			public const int Organizations = 2131099666;
+			// aapt resource value: 0x7f060006
+			public const int NeedVolunteersLabel = 2131099654;
 			
 			// aapt resource value: 0x7f06000c
-			public const int PhoneButton = 2131099660;
+			public const int NewsButton = 2131099660;
+			
+			// aapt resource value: 0x7f06000b
+			public const int NewsIconButton = 2131099659;
+			
+			// aapt resource value: 0x7f060003
+			public const int OrgImage = 2131099651;
+			
+			// aapt resource value: 0x7f060005
+			public const int OrgName = 2131099653;
+			
+			// aapt resource value: 0x7f06001a
+			public const int Organizations = 2131099674;
+			
+			// aapt resource value: 0x7f060014
+			public const int PhoneButton = 2131099668;
+			
+			// aapt resource value: 0x7f060007
+			public const int UniteButton = 2131099655;
+			
+			// aapt resource value: 0x7f060016
+			public const int WebsiteButton = 2131099670;
+			
+			// aapt resource value: 0x7f06000a
+			public const int linearLayout1 = 2131099658;
+			
+			// aapt resource value: 0x7f06000d
+			public const int linearLayout2 = 2131099661;
+			
+			// aapt resource value: 0x7f060001
+			public const int linearLayout3 = 2131099649;
+			
+			// aapt resource value: 0x7f060002
+			public const int relativeLayout1 = 2131099650;
 			
 			// aapt resource value: 0x7f060004
-			public const int UniteButton = 2131099652;
+			public const int relativeLayout2 = 2131099652;
 			
-			// aapt resource value: 0x7f06000e
-			public const int WebsiteButton = 2131099662;
+			// aapt resource value: 0x7f060010
+			public const int speechIconButton = 2131099664;
 			
 			private Id()
 			{
