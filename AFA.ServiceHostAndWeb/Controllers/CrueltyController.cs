c251f4d4ceee3a9915e7ff43edac50ad404d910d
pjacko <pdangelo22@gmail.com>
2013-02-26 22:32:02 -0500

    added report cruelty view to website

---
+++ AFA.ServiceHostAndWeb/Controllers/CrueltyController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AFA.ServiceHostAndWeb.Controllers
+{
+    public class CrueltyController : Controller
+    {
+        public ActionResult Report()
+        {
+            return View();
+        }
+
+    }
+}
